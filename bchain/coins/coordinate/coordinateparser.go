8ef6bb57b988c2af6c4934e511cf3c11d28262c6
sucildoss <sucil.jaya@gmail.com>
2025-08-04 19:58:55 +0530

    including coordinate parser for all network

--- bchain/coins/coordinate/coordinateparser.go
+++ bchain/coins/coordinate/coordinateparser.go
@@ -15,17 +15,31 @@ import (
 const (
 	// MainnetMagic is mainnet network constant
 	MainnetMagic wire.BitcoinNet = 0xf8bfb8d8
+	TestnetMagic wire.BitcoinNet = 0xf8beb9d8
+	RegtestMagic wire.BitcoinNet = 0xf8bfb8d8
 )
 
 var (
 	// MainNetParams are parser parameters for mainnet
 	MainNetParams chaincfg.Params
+	// TestNetParams are parser parameters for testnet
+	TestNetParams chaincfg.Params
+	// RegtestParams are parser parameters for regtest
+	RegtestParams chaincfg.Params
 )
 
 func init() {
 	MainNetParams = chaincfg.MainNetParams
 	MainNetParams.Net = MainnetMagic
 	MainNetParams.Bech32HRPSegwit = "ccrt"
+
+	TestNetParams = chaincfg.TestNetParams
+	TestNetParams.Net = TestnetMagic
+	TestNetParams.Bech32HRPSegwit = "tc"
+
+	RegtestParams = chaincfg.RegtestParams
+	RegtestParams.Net = RegtestMagic
+	RegtestParams.Bech32HRPSegwit = "cc"
 }
 
 // CoordinateParser handle
@@ -45,11 +59,21 @@ func NewCoordinateParser(params *chaincfg.Params, c *btc.Configuration) *Coordin
 func GetChainParams(chain string) *chaincfg.Params {
 	if !chaincfg.IsRegistered(&MainNetParams) {
 		err := chaincfg.Register(&MainNetParams)
+		if err == nil {
+			err = chaincfg.Register(&TestNetParams)
+		}
+		if err == nil {
+			err = chaincfg.Register(&RegtestParams)
+		}
 		if err != nil {
 			panic(err)
 		}
 	}
 	switch chain {
+	case "test":
+		return &TestNetParams
+	case "regtest":
+		return &RegtestParams
 	default:
 		return &MainNetParams
 	}
